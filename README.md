65938ec58a74bdbc7342809694085c96b28f3b70
Ilyah <0637047453t@gmail.com>
2023-02-04 19:38:24 +0200

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-# kowap.github.io
--- sw.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-importScripts('sw-toolbox.js');
-
-toolbox.precache(["index.html","dist/css/main.css","dist/js/main.js","dist/js/main.min.js"]);
-
-toolbox.router.get('/*', toolbox.networkFirst, {
-    networkTimeoutSeconds: 5
-});
